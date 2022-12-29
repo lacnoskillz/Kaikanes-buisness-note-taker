81da11afcdb55ea72d87e8e76ab79746e27efdd6
lacnoskillz <kaikane3457@hotmail.com>
2022-12-29 14:51:30 -0600

    added nodemon so i dont have to restart server everytime. attempted post methods

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-
+const noteData = require('./Develop/db/notes.json');
 const app = express();
 const PORT = 3001;
 
@@ -12,10 +12,33 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, 'Develop/public/index.html'))
 );
 
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'Develop/public/notes.html'))
-);
+app.get('/api/notes', (req, res) => {
+console.info(`GET /api/notes`);
+res.sendFile(path.join(__dirname, 'Develop/public/notes.html'))
+});
 
+
+app.post('/api/notes', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+  const { title, text } = req.body;
+  if(title && text){
+    const newNote = {
+      title,
+      text,
+      note_id: "random",
+    };
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('error posting');
+  }
+  
+  
+});
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
