d16def4db6efd4a166a4cd2abe1b052a547367b7
lacnoskillz <kaikane3457@hotmail.com>
2022-12-29 16:58:11 -0600

    fixed pathing issue with static public. downloaded uuid for note id. made readfile/write file functions to create new or delete notes

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const express = require('express');
 const path = require('path');
 const noteData = require('./Develop/db/notes.json');
 const app = express();
+const uuid = require('./helpers/uuid');
 const PORT = 3001;
+const fs = require('fs')
+const { v4: uuidv4 } = require('uuid')
 
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(express.static('Develop/public'));
 
 
 
@@ -12,33 +17,60 @@ app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, 'Develop/public/index.html'))
 );
 
-app.get('/api/notes', (req, res) => {
-console.info(`GET /api/notes`);
-res.sendFile(path.join(__dirname, 'Develop/public/notes.html'))
+app.get('/notes', (req, res) => {
+  console.info(`GET /api/notes`);
+  res.sendFile(path.join(__dirname, 'Develop/public/notes.html'))
 });
+
+app.get('/api/notes', (req, res) => {
+  fs.readFile("Develop/db/notes.json", "utf8", (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data))
+  })
+})
 
 
 app.post('/api/notes', (req, res) => {
   console.info(`${req.method} request received to add a note`);
   const { title, text } = req.body;
-  if(title && text){
+  if (title && text) {
     const newNote = {
       title,
       text,
-      note_id: "random",
-    };
-    const response = {
-      status: 'success',
-      body: newNote,
-    };
-    console.log(response);
-    res.status(201).json(response);
-  } else {
-    res.status(500).json('error posting');
+      id: uuidv4(),
+    }
+
+    fs.readFile("Develop/db/notes.json", "utf8", (err, data) => {
+      if (err) throw err;
+      let notesDb = JSON.parse(data)
+
+      notesDb.push(newNote)
+
+      fs.writeFile("Develop/db/notes.json", JSON.stringify(notesDb), (err) => {
+        if (err) throw err;
+        console.log("new note created")
+      })
+      res.sendFile(path.join(__dirname, 'Develop/public/notes.html'))
+    })
   }
-  
-  
 });
+
+app.delete("/api/notes/:id", (req, res) => {
+  let clickedNote = req.params.id
+  fs.readFile("Develop/db/notes.json", "utf8", (err, data) => {
+    if (err) throw err;
+    let notesDb = JSON.parse(data)
+
+    let newNotesDb = notesDb.filter(note => note.id !== clickedNote)
+
+    fs.writeFile("Develop/db/notes.json", JSON.stringify(newNotesDb), (err) => {
+      if (err) throw err;
+      console.log("noted deleted")
+    })
+    res.sendFile(path.join(__dirname, 'Develop/public/notes.html'))
+  })
+})
+
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
 );
